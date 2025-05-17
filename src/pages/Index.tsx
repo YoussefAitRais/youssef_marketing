cc093a573e7a74767c7c37c193e3439aa81bbe58
Ahmed Mouaden <mouaden.ahmed@ofppt-edu.ma>
2025-05-17 22:54:27 +0100

    finalizing the modifications

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -49,7 +49,6 @@ const Index = () => {
       <ProcessSection />
       <TestimonialsSection />
       <AboutSection />
-      <CtaSection />
       <BookCallSection />
       <Footer />
     </motion.div>
